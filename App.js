b9368553b03d5cdc6cf445eaf4aa9553a89c947e
Jesse Krim <jesse.krim@gmail.com>
2019-11-19 15:06:22 -0500

    Set up basic Navigator

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -14,18 +15,16 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.FEAFEAFEA to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+// const styles = StyleSheet.create({
+// container: {
+//   flex: 1,
+//   backgroundColor: '#fff',
+//   alignItems: 'center',
+//   justifyContent: 'center'
+// }
+// });
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FlatList, Text, View, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => {
+        <Text>{itemData.item.title}</Text>;
+      }}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
+};
+
+const styles = StyleSheet.create({
+  screen: {}
+});
+
+export default ProductsOverviewScreen;
