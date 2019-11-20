ac97d0efabdda8e124777e8219161c4188763fe0
Jesse Krim <jesse.krim@gmail.com>
2019-11-19 20:33:34 -0500

    Add items to cart, set up dispatched actions and reducers for cart

---
+++ store/reducers/cart.js
@@ -0,0 +1,37 @@
+import PRODUCTS from '../../data/dummy-data';
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
+
+const initialState = {
+  items: {},
+  totalAmount: 0
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.prodTitle;
+
+      let updatedOrNewCartItem;
+
+      if (state.items[addedProduct.id]) {
+        //already have item in the cart
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        //...state = not necessary to copy state as we update both state items
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice
+      };
+  }
+  return state;
+};
