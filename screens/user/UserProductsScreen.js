f7bffaf6e86d6dc843af920aa70bbd17caa1c771
Jesse Krim <jesse.krim@gmail.com>
2019-12-02 14:20:47 -0500

    Dynamically render ProductItem component buttons by refactoring ProductItem component to use props.children

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, Platform } from 'react-native';
+import { FlatList, Button, Platform } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
+import Colors from '../../constants/Colors';
 
 const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
@@ -19,14 +20,15 @@ const UserProductsScreen = props => {
           title={itemData.item.title}
           price={itemData.item.price}
           image={itemData.item.imageUrl}
-          onAddToCart={() => {}}
-        />
+          onSelect={() => {}}
+        >
+          <Button color={Colors.primary} title='Edit' onPress={() => {}} />
+          <Button color={Colors.primary} title='Delete' onPress={() => {}} />
+        </ProductItem>
       )}
     />
   );
 };
-
-const styles = StyleSheet.create({});
 
 UserProductsScreen.navigationOptions = navData => {
   return {
