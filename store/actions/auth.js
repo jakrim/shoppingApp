3e3aa28cf4702113d7fd37239f67baf8cc0f4631
Jesse Krim <jesse.krim@gmail.com>
2019-12-06 17:26:37 -0500

    Log users in functional

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
   return async dispatch => {
@@ -25,5 +26,33 @@ export const signup = (email, password) => {
     console.log(resData);
 
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBt3tslgRGwwYBINKFNRXXZQPnufRL9k8U',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong in Auth Actions');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({ type: LOGIN });
   };
 };
